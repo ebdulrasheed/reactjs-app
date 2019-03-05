cd9a3565db81a60b816aabf34678211bfd8f337a
ebdulrasheed <abdul.rasheed@citrusbits.com>
2019-03-05 15:11:30 +0500

    Edit card details

--- src/home.js
+++ src/home.js
@@ -10,7 +10,7 @@ class Home extends Component {
             <div>
                 <Card>
                     <CardBody>
-                        <CardTitle>Card title</CardTitle>
+                        <CardTitle>Abdul Rasheed Hassan Khan :)</CardTitle>
                         <CardSubtitle>Card subtitle</CardSubtitle>
                         <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                         <Button>Button</Button>
